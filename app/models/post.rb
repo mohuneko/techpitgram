12e29ac709813caf9f812e419cf3cbf40d988b3b
mohuneko <b0wdmohu@gmail.com>
2020-07-20 18:25:01 +0900

    [add]comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :photos, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   accepts_nested_attributes_for :photos
 
